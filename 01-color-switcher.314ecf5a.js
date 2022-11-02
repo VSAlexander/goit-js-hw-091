1de0e0df50f5f49cfbbc3915c6bcae7c83182526
VSAlexander <106761153+VSAlexander@users.noreply.github.com>
2022-11-02 20:06:18 +0000

    Deploying to gh-pages from @ VSAlexander/goit-js-hw-091@555501bcf892e59f67fcce5f5599dacc54d59176 🚀

---
+++ 01-color-switcher.314ecf5a.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),n=document.querySelector("body"),a=null;e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,a=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.314ecf5a.js.map
